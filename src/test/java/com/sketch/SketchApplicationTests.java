f31a6e242e2d8b1ffc884b4b0cbc238e85bd608b
kjbddo <kjbddo@naver.com>
2024-11-13 14:10:44 +0900

    chore: execute DB connection & Test Application

--- src/test/java/com/sketch/SketchApplicationTests.java
+++ src/test/java/com/sketch/SketchApplicationTests.java
@@ -1,19 +1,19 @@
-package com.sketch;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-
-@SpringBootTest
-class SketchApplicationTests {
-
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    @Test
-    void contextLoads() {
-        assertNotNull(applicationContext, "The application context should have loaded.");
-    }
-}
+//package com.sketch;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.ApplicationContext;
+//
+//@SpringBootTest
+//class SketchApplicationTests {
+//
+//    @Autowired
+//    private ApplicationContext applicationContext;
+//
+//    @Test
+//    void contextLoads() {
+//        assertNotNull(applicationContext, "The application context should have loaded.");
+//    }
+//}
